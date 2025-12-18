e8eda469dbe4cf289a76419869b23c5ecd04c1f7
Raul Ynzunza <raul@aviadalabs.com>
2025-12-18 11:23:04 -0700

    Second commit

--- public/web-components/public-room-configurator.js
+++ public/web-components/public-room-configurator.js
@@ -4,7 +4,7 @@ class SitecoreNextApp extends HTMLElement {
     this.initialized = true;
 
     const iframe = document.createElement('iframe');
-    iframe.src = this.getAttribute('src') || 'https://shure-room-configurator.vercel.app/';
+    iframe.src = this.getAttribute('src') || 'https://test-sitecore.vercel.app/';
     iframe.style.width = '100%';
     iframe.style.height = '100vh';
     iframe.style.border = '0';
